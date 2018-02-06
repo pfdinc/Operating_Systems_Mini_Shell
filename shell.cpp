38d2a514763fd5abdff15d0dafea67d031bfd370
Pushkin Feleke <pfdinc@gmail.com>
2018-02-05 19:23:46 -0800

    Update shell.cpp

--- shell.cpp
+++ shell.cpp
@@ -25,31 +25,24 @@ int main()
 	char *path2;
 	char *arr [250];
 	char *Tokenized ;
-
 	char input[250];
 	char *argv[TOKENSIZE];
 
 	while(true)
 	{
 		cout<< "cwushell-> ";
-
 		cin.getline(input,250);
 		StrTokenizer(input, argv);  
-
 		if (strcmp(input, "exit") == 0)  
 
 		{
 			break;
 		}
-
 		else if (strcmp(input, "\n") == 0){
-
 			continue;
 		}
-
 		myExecvp(argv);
 	}
-
 	return 0;
 }
 
@@ -58,13 +51,9 @@ void  myExecvp(char **argv)
 	pid_t  pid;
 	int    status;
 	int childStatus;
-
-
 	pid = fork();
-
 	if(pid == 0)
 	{
-
 		childStatus = execvp(*argv, argv);
 		if (childStatus < 0){
 			cout<<"ERROR:wrong input"<<endl;
@@ -74,14 +63,11 @@ void  myExecvp(char **argv)
 	}
 	else if(pid < 0)
 	{
-
 		cout<< "somthing went wrong!"<<endl;
-
 	}
 	else 
 
-	{   
-		int status;
+	{   int status;
 		waitpid(pid, &status , 0);
 /*
 			if(status == 0)
@@ -104,16 +90,11 @@ void  myExecvp(char **argv)
 void StrTokenizer(char *input, char **argv)
 {
 	char *stringTokenized;
-
 	stringTokenized = strtok(input, " ");
-
 	while(stringTokenized != NULL)
 	{
-
 		*argv++  = stringTokenized;
 		stringTokenized = strtok(NULL, " ");
-
-
 	}
 
 	*argv = NULL;
@@ -124,18 +105,13 @@ void StrTokenizer(char *input, char **argv)
 int GetEnv()
 {
 	char *path2;
-
 	char *arr2[250];
-
 	char *Tokenized ;
 	path2 = getenv("PATH");
-
 	Tokenized = strtok(path2, ":");
 	int k = 0;
-
 	while(Tokenized != NULL)
 	{
-
 		arr2[k] = Tokenized;
 		Tokenized = strtok(NULL, ":");
 		k++;
@@ -144,8 +120,6 @@ int GetEnv()
 	}
 //cout<< "line 46" <<endl;
 	arr2[k] = NULL;
-
-
 }
 
 
